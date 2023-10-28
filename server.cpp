760698024b2c6b28d1639075184492abdf337b1e
vanPThomas <thomas.van.petegem@hotmail.com>
2023-10-28 21:14:28 +0200

    buffer gets cleared after every loop

--- server.cpp
+++ server.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <unistd.h>
+#include <cstring>
 
 int main()
 {
@@ -10,7 +11,7 @@ int main()
     int connSocked = socket(AF_INET, SOCK_STREAM, 0); // AF_INET: ipv4 | SOCK_STREAM: TCP, two way reliable communication
     if (connSocked == -1)
     {
-        std::cout << "Failed to create socket, errno: " << errno << std::endl;
+        std::cout << "Failed to create socket, errno: " << errno << "\n";
         exit(EXIT_FAILURE);
     }
 
@@ -26,14 +27,14 @@ int main()
     // assign an IP address and port to the socket
     if (bind(connSocked, (struct sockaddr *)&sockaddr, sizeof(sockaddr)) < 0)
     {
-        std::cout << "Failed to bind to port " << portNR << ", errno: " << errno << std::endl;
+        std::cout << "Failed to bind to port " << portNR << ", errno: " << errno << "\n";
         exit(EXIT_FAILURE);
     }
 
     // listen for connection, max 10 connections
     if (listen(connSocked, 10) < 0)
     {
-        std::cout << "Failed to listen on socket, errno" << errno << std::endl;
+        std::cout << "Failed to listen on socket, errno" << errno << "\n";
         exit(EXIT_FAILURE);
     }
 
@@ -42,24 +43,27 @@ int main()
     int connection = accept(connSocked, (struct sockaddr *)&sockaddr, (socklen_t *)&addrlen);
     if (connection < 0)
     {
-        std::cout << "Failure to grab connection, errno: " << errno << std::endl;
+        std::cout << "Failure to grab connection, errno: " << errno << "\n";
         exit(EXIT_FAILURE);
     }
 
     // read from connection
-    char *buffer = new char[100];
-    const char *exitCode = "EXIT";
+    char buffer[100];
+    const char *exitCode = "*EXIT*";
     const int len = 4;
 
     while (!std::equal(exitCode, exitCode + len, buffer))
     {
+        // clear old buffer
+        memset(&buffer[0], 0, sizeof(buffer));
+        // catch new incomming message
         auto bytesRead = read(connection, buffer, 100);
 
         std::cout << "The Message was: " << buffer << "\n";
     }
 
     // send message to the connection
-    std::string response = "Good talking to you\n";
+    std::string response = "Server Shutting Down";
     send(connection, response.c_str(), response.size(), 0);
 
     // close connection
