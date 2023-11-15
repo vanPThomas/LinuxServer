c09f8240e91a143ec536ddbefc8fd955477614ea
vanPThomas <thomas.van.petegem@hotmail.com>
2023-11-15 18:44:33 +0100

    Send messages to all connected

--- server.cpp
+++ server.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <unistd.h>
 #include <cstring>
-#include "story.h"
 #include "chat.h"
 #include <sys/types.h>
 #include <arpa/inet.h>
@@ -14,20 +13,15 @@
 #define FALSE 0
 
 void printMenu(int connection);
+void closeClientSocket(int clientSocket[], int index);
+int initializeServerSocket(int port);
 
 int main()
 {
     // create socket
-    int masterSocket = socket(AF_INET, SOCK_STREAM, 0); // AF_INET: ipv4 | SOCK_STREAM: TCP, two way reliable communication
-    if (masterSocket == -1)
-    {
-        std::cout << "Failed to create socket, errno: " << errno << "\n";
-        exit(EXIT_FAILURE);
-    }
 
-    int opt = TRUE;
-    int clientSocket[30];
     int maxClients = 30;
+    int clientSocket[maxClients];
     int newSocket;
     int valread;
     int sd;
@@ -37,14 +31,13 @@ int main()
     int PORT;
     int addrlen;
     sockaddr_in address;
-    char buffer[1024];
+    const int bufferSize = 1024;
+    char buffer[bufferSize];
 
-    // listen at chosen port on any address
     std::cout << "Enter Port: ";
     std::cin >> PORT;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT); // hton: convert int to network byte order
+
+    int masterSocket = initializeServerSocket(PORT); // AF_INET: ipv4 | SOCK_STREAM: TCP, two way reliable communication
 
     // initialize all sockets to 0, not checked
     for (int i = 0; i < maxClients; i++)
@@ -52,30 +45,7 @@ int main()
         clientSocket[i] = 0;
     }
 
-    // set master socket to allow multiple connections
-    if (setsockopt(masterSocket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0)
-    {
-        std::cout << "Failed to setsockopt\n";
-        exit(EXIT_FAILURE);
-    }
-
-    // assign an IP address and port to the socket
-    if (bind(masterSocket, (struct sockaddr *)&address, sizeof(address)) < 0)
-    {
-        std::cout << "Failed to bind to port " << PORT << ", errno: " << errno << "\n";
-        exit(EXIT_FAILURE);
-    }
-
-    std::cout << "Listening on port " << PORT << "\n";
-
-    // listen for connection, max 10 connections
-    if (listen(masterSocket, 10) < 0)
-    {
-        std::cout << "Failed to listen on socket, errno" << errno << "\n";
-        exit(EXIT_FAILURE);
-    }
-
-    // garb connection
+    // grab connection
     addrlen = sizeof(address);
 
     std::cout << "Waiting for connections ... \n";
@@ -134,9 +104,9 @@ int main()
         {
             sd = clientSocket[i];
 
-            if (FD_ISSET(sd, &readfds))
+            if (FD_ISSET(sd, &readfds)) // check if there is data to be read
             {
-                valread = read(sd, buffer, 1024);
+                valread = read(sd, buffer, bufferSize);
                 if (valread == 0)
                 {
                     // if somebody disconnects, get their info
@@ -145,70 +115,25 @@ int main()
                     std::cout << "Host Disconnected! ip: " << inet_ntoa(address.sin_addr) << " port: " << ntohs(address.sin_port) << "\n";
 
                     // close and mark as 0
-                    close(sd);
-                    clientSocket[i] = 0;
+                    closeClientSocket(clientSocket, i);
                 }
                 else
                 {
                     buffer[valread] = '\0';
                     std::cout << buffer << "\n";
-                    send(sd, buffer, strlen(buffer), 0);
+                    // send(sd, buffer, strlen(buffer), 0);
+                    for (int sdTarget : clientSocket)
+                    {
+                        if (sdTarget != 0 && sdTarget != sd)
+                        {
+
+                            send(sdTarget, buffer, strlen(buffer), 0);
+                        }
+                    }
                 }
             }
         }
     }
-
-    /*
-    int connection = accept(masterSocket, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-    if (connection < 0)
-    {
-        std::cout << "Failure to grab connection, errno: " << errno << "\n";
-        exit(EXIT_FAILURE);
-    }
-
-    // read from connection
-    char buffer[1024];
-    const char *exitCode = "*EXIT*";
-    const int exitLen = 6;
-    const char *storyCode = "*STORY*";
-    const int storyLen = 7;
-    const char *chatCode = "*CHAT*";
-    const int chatLen = 6;
-
-    printMenu(connection);
-
-    Chat c;
-    while (!std::equal(exitCode, exitCode + exitLen, buffer))
-    {
-        // clear old buffer
-        memset(&buffer[0], 0, sizeof(buffer));
-
-        // catch new incomming message
-        auto bytesRead = read(connection, buffer, 100);
-
-        if (std::equal(storyCode, storyCode + storyLen, buffer))
-        {
-            Story s;
-            s.TestPrint(connection);
-        }
-        else if (std::equal(chatCode, chatCode + chatLen, buffer))
-        {
-            c.addConnectionToChat(connection);
-            c.enterChat(connection);
-        }
-
-        std::cout << "The Message was: " << buffer << "\n";
-    }
-
-    // send message to the connection
-    std::string response = "Server Shutting Down\n";
-    send(connection, response.c_str(), response.size(), 0);
-
-    // close connection
-    close(connection);
-    close(masterSocket);
-    */
-
     return 0;
 }
 
@@ -219,4 +144,55 @@ void printMenu(int connection)
     response += "*STORY* : get a story from the server\n";
     response += "*CHAT* : Enter chat\n";
     send(connection, response.c_str(), response.size(), 0);
+}
+
+void closeClientSocket(int clientSocket[], int index)
+{
+    close(clientSocket[index]);
+    clientSocket[index] = 0;
+}
+
+void handleSystemCallError(std::string errorMsg)
+{
+    std::cout << errorMsg << ", errno: " << errno << "\n";
+    exit(EXIT_FAILURE);
+}
+
+int initializeServerSocket(int port)
+{
+    int maxConnectionAmount = 30;
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0); // AF_INET: ipv4 | SOCK_STREAM: TCP, two way reliable communication
+    if (serverSocket == -1)
+    {
+        handleSystemCallError("Failed to create socket");
+    }
+
+    int opt = TRUE;
+    // set master socket to allow multiple connections
+
+    if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0)
+    {
+        handleSystemCallError("Failed to setsockopt");
+    }
+
+    sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port); // hton: convert int to network byte order
+
+    // assign an IP address and port to the socket
+    if (bind(serverSocket, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        handleSystemCallError("Failed to bind to port");
+    }
+
+    std::cout << "Listening on port " << port << "\n";
+
+    // listen for connection, max 10 connections
+    if (listen(serverSocket, maxConnectionAmount) < 0)
+    {
+        handleSystemCallError("Failed to listen on socket");
+    }
+
+    return serverSocket;
 }
